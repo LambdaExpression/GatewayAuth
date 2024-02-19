3997845984016e52546909f40074fb6c120bcabc
LambdaExpression <13425088053@163.com>
2024-02-19 17:20:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,6 +17,8 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X 'main.GoVersion=$(go
 mv main ./build/gatewayAuth_linux_amd64_$version
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X 'main.GoVersion=$(go version)' -X main.GitCommit=`git rev-parse HEAD` -X 'main.BuildTime=`date +"%Y-%m-%d %H:%m:%S"`'" ./src/main.go
 mv main ./build/gatewayAuth_darwin_amd64_$version
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-X 'main.GoVersion=$(go version)' -X main.GitCommit=`git rev-parse HEAD` -X 'main.BuildTime=`date +"%Y-%m-%d %H:%m:%S"`'" ./src/main.go
+mv main ./build/gatewayAuth_darwin_arm64_$version
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-X 'main.GoVersion=$(go version)' -X main.GitCommit=`git rev-parse HEAD` -X 'main.BuildTime=`date +"%Y-%m-%d %H:%m:%S"`'" ./src/main.go
 mv main.exe ./build/gatewayAuth_windows_amd64_$version.exe
 
