94d3cc7530ec773f3c65c8d312a72a1536789001
LambdaExpression <LambdaExpression@163.com>
2024-02-20 09:43:56 +0800

    feature: update version

--- src/main.go
+++ src/main.go
@@ -16,7 +16,7 @@ import (
 var configPath string
 var printVersion bool
 
-var Version = "v1.0.1"
+var Version = "v1.0.2"
 var GoVersion = "not set"
 var GitCommit = "not set"
 var BuildTime = "not set"
